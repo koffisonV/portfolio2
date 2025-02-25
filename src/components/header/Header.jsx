672e12a7c03b3b2810066d0efe3b7481cf90e0fd
Koffison Voumadi <koffison29@gmail.com>
2025-02-24 21:56:21 -0500

    removed cv

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CTA from "./cta";
+// import CTA from "./cta";
 import ME from "../../assets/me.jpg";
 import HeaderSocials from "./HeaderSocials";
 
@@ -13,7 +13,7 @@ const Header = () => {
 
         {/* CTA Section */}
         <div className="mt-10">
-          <CTA />
+          {/* <CTA /> */}
         </div>
 
         {/* Social Media Links */}
